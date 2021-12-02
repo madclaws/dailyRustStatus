cb1a2f3d447bb516ee676686f311a1c83e660d2f
madclaws <pavanan.anandu63@gmail.com>
2021-12-02 23:24:11 +0530

    feat: robot-simulator

--- robot-simulator/src/main.rs
+++ robot-simulator/src/main.rs
@@ -1,6 +1,6 @@
 use robot_simulator::*;
 fn main() {
-    let mut robot = Robot::new(0, 0, Direction::North);
-    robot.instructions("LAAARALA");
+    let robot = Robot::new(0, 0, Direction::North).instructions("LAAARALA");
+    // robot.instructions("LAAARALA");
     println!("{:?}", robot.direction());
 }
