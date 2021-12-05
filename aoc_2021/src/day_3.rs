9b930417c005dd9f55c85306aefee3dddb8b7c7c
madclaws <pavanan.anandu63@gmail.com>
2021-12-06 02:26:21 +0530

    feat: :beers: femo#60 => Binary Diagnostic: AOC 3_2

--- aoc_2021/src/day_3.rs
+++ aoc_2021/src/day_3.rs
@@ -1,12 +1,25 @@
 use std::fs;
 use std::collections::HashMap;
-
+use std::collections::HashSet;
 #[derive(Debug)]
 pub struct BitCount {
     zero_count: i32,
     one_count: i32,
 }
 
+impl BitCount {
+    fn new() -> Self {
+        BitCount{zero_count: 0, one_count: 0}
+    }
+
+    fn increase_zero_count(&mut self) {
+        self.zero_count += 1
+    }
+
+    fn increase_one_count(&mut self) {
+        self.one_count += 1
+    }
+}
 pub fn run() {
     println!("\nDay 3 => Binary Diagnostic");
     let raw_report = fs::read_to_string("/home/madclaws/labs/aoc_2021/inputs/day_3.txt").expect("Unable to load file");
@@ -14,7 +27,7 @@ pub fn run() {
     let mut column_wise_aggregate: HashMap<i32, BitCount> = HashMap::new();
     let report_lines: Vec<&str> = raw_report.split('\n').collect();
     
-    for report_line in report_lines {
+    for report_line in &report_lines {
         for (index, binary) in report_line.chars().enumerate() {
             column_wise_aggregate.entry(index as i32)
             .and_modify(|bitcount| {
@@ -34,6 +47,14 @@ pub fn run() {
     let epsilon_rate = create_epsilon_rate_in_binary(&colum_agg_vec);
     println!("Power consumption of submarine => {}", convert_binary_to_decimal(gamma_rate)
          * convert_binary_to_decimal(epsilon_rate));
+
+    let oxygen_rate = calculate_oxygen_generator_rate(&report_lines, &colum_agg_vec, report_lines[0].len());
+    println!("oxygen rate => {}\n\n", oxygen_rate);
+    
+    let carbon_scrubber_rate = calculate_carbon_scrubber_rate(&report_lines, &colum_agg_vec, report_lines[0].len());
+    println!("carbon_scrubber_rate => {}", carbon_scrubber_rate);
+
+    println!("Life support rating of submarine => {}", oxygen_rate * carbon_scrubber_rate);
 }
 
 fn create_gamma_rate_in_binary(column_aggregate: &[(i32, BitCount)]) -> String {
@@ -79,4 +100,111 @@ fn convert_binary_to_decimal(binary: String) -> u32 {
         decimal_number += element * u32::pow(2, power);
     }
     decimal_number
+}
+
+fn calculate_oxygen_generator_rate(report_lines: &[&str], column_agg: &[(i32, BitCount)], line_len: usize) -> u32{
+    let mut filtered_set: HashSet<String> = HashSet::new();   
+    
+    for pos in 0..line_len {
+        let bitcount = get_bitcount(&filtered_set, pos);
+        for &report_line in report_lines{
+            if pos > 0 && !filtered_set.contains(report_line) {
+                continue;
+            }
+            let most_common_bit;
+            if pos > 0 {
+                most_common_bit = get_most_common_bit_by_struct(&bitcount);
+            } else {
+                most_common_bit = get_most_common_bit(&column_agg[pos as usize]); 
+            }
+            if most_common_bit == report_line.chars().nth(pos as usize).unwrap() {
+                filtered_set.insert(report_line.to_owned());
+            } else if pos > 0{
+                filtered_set.remove(report_line);
+            }
+        }
+        if filtered_set.len() == 1 {
+            break;
+        }
+    }
+    let oxygen_list: Vec<String> = filtered_set.into_iter().collect();
+    println!("{:?}", oxygen_list[0]);
+    convert_binary_to_decimal(oxygen_list[0].clone()) 
+}
+
+fn calculate_carbon_scrubber_rate(report_lines: &[&str], column_agg: &[(i32, BitCount)], line_len: usize) -> u32{
+    let mut filtered_set: HashSet<String> = HashSet::new();   
+    
+    for pos in 0..line_len {
+        let bitcount = get_bitcount(&filtered_set, pos);
+        for &report_line in report_lines{
+            if pos > 0 && !filtered_set.contains(report_line) {
+                continue;
+            }
+            let least_common_bit;
+            if pos > 0 {
+                least_common_bit = get_least_common_bit_by_struct(&bitcount);
+            } else {
+                least_common_bit = get_least_common_bit(&column_agg[pos as usize]); 
+            }
+            
+            if least_common_bit == report_line.chars().nth(pos as usize).unwrap() {
+                filtered_set.insert(report_line.to_owned());
+            } else if pos > 0{
+                filtered_set.remove(report_line);
+            }
+        }
+        if filtered_set.len() == 1 {
+            break;
+        }
+    }
+    let carbonlist: Vec<String> = filtered_set.into_iter().collect();
+    println!("{:?}", carbonlist[0]);
+    convert_binary_to_decimal(carbonlist[0].clone()) 
+}
+
+fn get_most_common_bit(column_agg: &(i32, BitCount)) -> char {
+    let bit_count = &column_agg.1;
+    if bit_count.zero_count > bit_count.one_count {
+        '0'
+    } else {
+        '1'
+    }
+}
+
+fn get_most_common_bit_by_struct(bitcount: &BitCount) -> char {
+    if bitcount.zero_count > bitcount.one_count {
+        '0'
+    } else {
+        '1'
+    }
+}
+
+fn get_least_common_bit(column_agg: &(i32, BitCount)) -> char {
+    let bit_count = &column_agg.1;
+    if bit_count.zero_count > bit_count.one_count {
+        '1'
+    } else {
+        '0'
+    }
+}
+
+fn get_least_common_bit_by_struct(bitcount: &BitCount) -> char {
+    if bitcount.zero_count > bitcount.one_count {
+        '1'
+    } else {
+        '0'
+    }
+}
+
+fn get_bitcount(filtered_set: &HashSet<String>, pos: usize) -> BitCount{
+    let mut bitcount: BitCount = BitCount::new();
+    for key in filtered_set {
+        if key.chars().nth(pos).unwrap() == '0' {
+            bitcount.increase_zero_count();
+        } else {
+            bitcount.increase_one_count();
+        }
+    }
+    bitcount
 }
--- readme.md
+++ readme.md
@@ -63,3 +63,4 @@ Writing Rust daily for building reliable and efficient software #FEMO.
 57. [Dive - AOC: 2](https://github.com/madclaws/dailyRustStatus/tree/master/aoc_2021/src/day_2.rs)
 58. [Resistor Color](https://github.com/madclaws/dailyRustStatus/tree/master/resistor-color)
 59. [Binary Diagnostic - AOC: 3_1](https://github.com/madclaws/dailyRustStatus/tree/master/aoc_2021/src/day_3.rs)
+60. [Binary Diagnostic - AOC: 3_2](https://github.com/madclaws/dailyRustStatus/tree/master/aoc_2021/src/day_3.rs)
