acf9e50ae3d8d5b8cc80a33f1f10d7c8c5bc1c6c
madclaws <pavanan.anandu63@gmail.com>
2021-12-17 22:03:42 +0530

    feat: femo#71 => Saddle points

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -1,6 +1,70 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    unimplemented!(
-        "find the saddle points of the following matrix: {:?}",
-        input
-    )
+    let mut saddle_pts: Vec<(usize, usize)> = Vec::new();
+    let mut greater_row_indices: Vec<(usize, usize, usize)> = Vec::new();
+    let mut smallest_col_indices: Vec<(usize, usize, usize)> = Vec::new();
+
+    for i in 0..input.len() {
+        let mut largest_row_index: Option<(usize, usize, usize)> = None;
+        for j in 0..input[i].len() {
+            if largest_row_index.is_none() {
+                largest_row_index = Some((i, j, input[i][j] as usize));
+            } else {
+                if largest_row_index.unwrap().2 < input[i][j] as usize {
+                    largest_row_index = Some((i, j, input[i][j] as usize));
+                } 
+            }
+        }
+        if let Some(index_info) = largest_row_index {
+
+            for index in 0..input.len() {
+                if input[index_info.0][index] == index_info.2 as u64 {
+                    greater_row_indices.push((
+                        index_info.0,
+                        index,
+                        input[index_info.0][index] as usize
+                    ));
+                }
+            }
+        }
+    }
+
+    // For every column
+    for j in 0..input.len() {
+        let mut smallest_col_index: Option<(usize, usize, usize)> = None;
+        for i in 0..input[j].len() {
+            if smallest_col_index.is_none() {
+                smallest_col_index = Some((i, j, input[i][j] as usize));
+            } else {
+                if smallest_col_index.unwrap().2 > input[i][j] as usize {
+                    smallest_col_index = Some((i, j, input[i][j] as usize));
+                } 
+            }
+        }
+
+        if smallest_col_index.is_some() {
+            for index in 0..input[smallest_col_index.unwrap().0].len() {
+                if input[index][smallest_col_index.unwrap().1] == smallest_col_index.unwrap().2 as u64{
+                    if is_saddle(&greater_row_indices, (index, smallest_col_index.unwrap().1, input[index][smallest_col_index.unwrap().1] as usize)) {
+                        smallest_col_indices.push(smallest_col_index.unwrap());
+                        saddle_pts.push((index, smallest_col_index.unwrap().1))
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{:?}", greater_row_indices);
+    println!("{:?}", smallest_col_indices);
+    println!("{:?}", saddle_pts);
+
+    saddle_pts
+}
+
+fn is_saddle(greater_row_indices: &[(usize, usize, usize)], small_col_index: (usize, usize, usize)) -> bool {
+    for index in 0..greater_row_indices.len() {
+        if small_col_index.0 == greater_row_indices[index].0 && small_col_index.1 == greater_row_indices[index].1 {
+            return true
+        }
+    }
+    return false
 }
--- saddle-points/tests/saddle-points.rs
+++ saddle-points/tests/saddle-points.rs
@@ -14,7 +14,7 @@ fn identify_single_saddle_point() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn identify_empty_matrix() {
     let input = vec![vec![], vec![], vec![]];
     let expected: Vec<(usize, usize)> = Vec::new();
@@ -22,7 +22,7 @@ fn identify_empty_matrix() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn identify_lack_of_saddle_point() {
     let input = vec![vec![1, 2, 3], vec![3, 1, 2], vec![2, 3, 1]];
     let expected: Vec<(usize, usize)> = Vec::new();
@@ -30,7 +30,7 @@ fn identify_lack_of_saddle_point() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn multiple_saddle_points_in_col() {
     let input = vec![vec![4, 5, 4], vec![3, 5, 5], vec![1, 5, 4]];
     assert_eq!(
@@ -40,7 +40,7 @@ fn multiple_saddle_points_in_col() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn multiple_saddle_points_in_row() {
     let input = vec![vec![6, 7, 8], vec![5, 5, 5], vec![7, 5, 6]];
     assert_eq!(
@@ -50,7 +50,7 @@ fn multiple_saddle_points_in_row() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn identify_bottom_right_saddle_point() {
     let input = vec![vec![8, 7, 9], vec![6, 7, 6], vec![3, 2, 5]];
     assert_eq!(vec![(2, 2)], find_saddle_points(&input));
@@ -58,7 +58,7 @@ fn identify_bottom_right_saddle_point() {
 
 // track specific as of v1.3
 #[test]
-#[ignore]
+// #[ignore]
 fn non_square_matrix_high() {
     let input = vec![vec![1, 5], vec![3, 6], vec![2, 7], vec![3, 8]];
     assert_eq!(vec![(0, 1)], find_saddle_points(&input));
@@ -104,3 +104,5 @@ fn identify_all_saddle_points() {
         find_sorted_saddle_points(&input)
     );
 }
+
+
