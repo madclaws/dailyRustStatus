487bbfbbadc9cf9ed2392eae53429789bbfe4a70
madclaws <pavanan.anandu63@gmail.com>
2022-08-10 11:56:31 +0530

    feat: added score for openframe

--- bowling/src/lib.rs
+++ bowling/src/lib.rs
@@ -7,19 +7,36 @@ pub enum Error {
 pub struct BowlingGame {
     score: u16,
     current_frame: u16,
-    frame_list: Vec<u16>
+    sub_frames: Vec<u16>,
+    is_game_completed: bool
 }
 
 impl BowlingGame {
     pub fn new() -> Self {
-        BowlingGame{score: 0, current_frame: 0, frame_list: Vec::new()}
+        BowlingGame{
+            score: 0, current_frame: 0, sub_frames: Vec::new(),
+            is_game_completed: false
+        }
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
-        unimplemented!("Record that {} pins have been scored", pins);
+        // open frame 
+        if pins < 10 {
+            // score should be stored for current subframe
+            self.sub_frames.push(pins);
+            // handle for existing spare
+            // handle for existing streak
+        }
+        // spare
+        // streak
+        Ok(())
     }
 
     pub fn score(&self) -> Option<u16> {
-        unimplemented!("Return the score if the game is complete, or None if not.");
+        if self.is_game_completed {
+            None
+        } else {
+            Some(1)
+        }
     }
 }
