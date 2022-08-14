cb7de8df0ecd41adf1c6ca3fd30fe0edc0d325a5
madclaws <pavanan.anandu63@gmail.com>
2022-08-14 11:04:04 +0530

    feat: handling spare done

--- bowling/src/lib.rs
+++ bowling/src/lib.rs
@@ -8,6 +8,7 @@ pub struct BowlingGame {
     current_frame: u16,
     frames: Vec<Frame>,
     is_game_completed: bool,
+    spare_frame: Option<u16>
 }
 
 #[derive(Clone)]
@@ -30,29 +31,50 @@ impl Frame {
 impl BowlingGame {
     pub fn new() -> Self {
         BowlingGame {
+            spare_frame: None,
             current_frame: 0,
             frames: vec![Frame::new(); 10],
             is_game_completed: false,
         }
     }
 
+    /// TODO
+    /// - Handle open frame - Done
+    /// - Handle spare - DONE
+    /// - Handle streak
+    /// - Handle 10th frame edge case
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
         // open frame
         if self.current_frame < 10 {
-            let current_frame = &mut self.frames[self.current_frame as usize];
-            current_frame.sub_frames[current_frame.current_subframe as usize] = pins;
-            if current_frame.current_subframe == 1 {
-                current_frame.total = current_frame.sub_frames.iter().sum();
-                self.current_frame += 1;
-            } else {
-                current_frame.current_subframe += 1
+            let frame_index = self.current_frame;
+            {
+                let current_frame = &mut self.frames[self.current_frame as usize];
+                current_frame.sub_frames[current_frame.current_subframe as usize] = pins;
+                if current_frame.current_subframe == 1 {
+                    current_frame.total = current_frame.sub_frames.iter().sum();
+                    if current_frame.total == 10 {
+                        println!("Spare on frame {}", self.current_frame);
+                        self.spare_frame = Some(self.current_frame)
+                    }
+                    self.current_frame += 1;
+                } else {
+                    current_frame.current_subframe += 1
+                }
+            }
+            
+            // This means the roll was for first sub_frame
+            if frame_index == self.current_frame {
+                 // // Check if a spare exists, if then update spareframe total_score
+                if let Some(frame) = self.spare_frame {
+                    println!("A spare exists at frame {}", frame);
+                    self.frames[frame as usize].total += pins;
+                    self.spare_frame = None;
+                }
             }
 
             if self.current_frame == 10 {
                 self.is_game_completed = true;    
             }
-        } else {
-            self.is_game_completed = true;
         }
         Ok(())
     }
--- bowling/src/main.rs
+++ bowling/src/main.rs
@@ -2,11 +2,11 @@ use bowling::*;
 
 fn main() {
     let mut bowling = BowlingGame::new();
-    bowling.roll(3);
     bowling.roll(2);
+    bowling.roll(8);
 
-    bowling.roll(3);
-    bowling.roll(2);
+    bowling.roll(5);
+    bowling.roll(5);
 
     bowling.roll(3);
     bowling.roll(2);
