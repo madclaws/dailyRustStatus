81a9b2d40c19bdf5ac089020f0b9a4e0bc32fcf1
madclaws <pavanan.anandu63@gmail.com>
2022-08-21 15:17:39 +0530

    feat: earning strike skips to next frame

--- bowling/src/lib.rs
+++ bowling/src/lib.rs
@@ -56,91 +56,7 @@ impl BowlingGame {
             strike_frames: Vec::new(),
         }
     }
-
-    /// TODO
-    /// - Handle open frame - Done
-    /// - Handle spare - DONE
-    /// - Handle streak - DONE
-    /// - Handle 10th frame edge case
-    pub fn roll_old(&mut self, pins: u16) -> Result<(), Error> {
-        // open frame
-        if self.current_frame < 10 {
-            let frame_index = self.current_frame;
-            let mut is_current_frame_strike = false;
-            {
-                let current_frame = &mut self.frames[self.current_frame as usize];
-                if pins != 10 {
-                    current_frame.sub_frames[current_frame.current_subframe as usize] = pins;
-                }
-                if current_frame.current_subframe == 1 {
-                    current_frame.total = current_frame.sub_frames.iter().sum();
-                    if current_frame.total == 10 {
-                        println!("Spare on frame {}", self.current_frame);
-                        self.spare_frame = Some(self.current_frame)
-                    }
-                    self.current_frame += 1;
-
-                    for strike_frame in &mut self.strike_frames {
-                        if strike_frame.active {
-                            println!("Strike frame exists at {}", strike_frame.frame_index);
-                            strike_frame.next_rolls.push(pins);
-                            if strike_frame.next_rolls.len() == 2 {
-                                strike_frame.active = false;
-                                let total_frame_score: u16 = strike_frame.next_rolls.iter().sum();
-                                self.frames[strike_frame.frame_index as usize].total =
-                                    total_frame_score + 10;
-                            }
-                        }
-                    }
-                } else {
-                    if pins == 10 {
-                        // A strike
-                        println!("Strike on frame {}", self.current_frame);
-                        let strike_frame = StrikeFrame::new(self.current_frame);
-                        self.strike_frames.push(strike_frame);
-                        is_current_frame_strike = true;
-                        // updating streak score on first sub-frame
-                        // self.frames[self.current_frame as usize].total = pins;
-                        self.current_frame += 1;
-                    } else {
-                        current_frame.current_subframe += 1
-                    }
-                }
-            }
-
-            // This means the roll was for first sub_frame
-            if frame_index == self.current_frame || is_current_frame_strike {
-                // // Check if a spare exists, if then update spareframe total_score
-                if let Some(frame) = self.spare_frame {
-                    println!("A spare exists at frame {}", frame);
-                    self.frames[frame as usize].total += pins;
-                    self.spare_frame = None;
-                }
-                // find the active strike frames and update them
-                for strike_frame in &mut self.strike_frames {
-                    if strike_frame.active && !is_current_frame_strike {
-                        println!("Strike frame exists at {}", strike_frame.frame_index);
-                        strike_frame.next_rolls.push(pins);
-                        println!("Pins added to strike frame {}", pins);
-                        if strike_frame.next_rolls.len() == 2 {
-                            strike_frame.active = false;
-                            let total_frame_score: u16 = strike_frame.next_rolls.iter().sum();
-                            self.frames[strike_frame.frame_index as usize].total =
-                                total_frame_score + 10;
-                        }
-                    }
-                }
-            }
-
-            if self.current_frame == 10 {
-                self.is_game_completed = true;
-            }
-            Ok(())
-        } else {
-            return Err(Error::GameComplete);
-        }
-    }
-
+    
     pub fn score(&self) -> Option<u16> {
         if self.is_game_completed {
             let mut total_score: u16 = 0;
@@ -159,7 +75,7 @@ impl BowlingGame {
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
         // We get the valid frame, subframe to enter the points.
         // It will be an Option
-        if let Some(current_frame) = self.get_valid_frame() {
+        if let Some(current_frame) = self.get_valid_frame(pins) {
             self.current_frame = current_frame;
             if self.is_pin_valid(pins) {
                 self.frames[current_frame as usize].sub_frames.push(pins);
@@ -167,19 +83,43 @@ impl BowlingGame {
                     && self.frames[current_frame as usize].sub_frames.len() == 2
                 {
                     // calculate the total score if frame is finished
-                    self.frames[current_frame as usize].total =
-                        self.frames[current_frame as usize].sub_frames.iter().sum();
+                    self.frames[current_frame as usize].total = self.get_subframes_sum(current_frame);
                 } else if self.current_frame == 9
                     && self.frames[current_frame as usize].sub_frames.len() >= 2
                 {
-                    self.frames[current_frame as usize].total =
-                        self.frames[current_frame as usize].sub_frames.iter().sum();
+                    self.frames[current_frame as usize].total = self.get_subframes_sum(current_frame)
                 } else {
                     // if sub_frame is 0, then check if previous frame is spare, if yes, then add current pin
                     // to its total score.
                     if self.current_frame > 0 && self.is_spare(self.current_frame - 1) {
                         println!("SPARE FRAME {}", self.current_frame);
                         self.frames[(self.current_frame - 1) as usize].total += pins
+                    }
+                    // check if an active strikeframe is there.., then add the pins to its nextrolls
+                    let mut strike_index: i16 = -1;
+                    if let Some(strike_frame) = self.get_strike_frame() {
+                        strike_index = strike_frame.frame_index as i16;
+                    }
+                    println!("Active strike index => {}", strike_index);
+                    if strike_index >= 0 {
+                        self.strike_frames[strike_index as usize]
+                            .next_rolls
+                            .push(pins);
+                        let frame_index = self.strike_frames[strike_index as usize].frame_index;
+                        if self.strike_frames[strike_index as usize].next_rolls.len() == 2 {
+                            self.strike_frames[strike_index as usize].active = false;
+                            self.frames[frame_index as usize].total = self.strike_frames
+                                [strike_index as usize]
+                                .next_rolls
+                                .iter()
+                                .sum::<u16>()
+                                + 10;
+                        }
+                    }
+                    // strike on first sub_frame
+                    if pins == 10 {
+                        self.strike_frames
+                            .push(StrikeFrame::new(self.current_frame))
                     }
                 }
             } else {
@@ -202,10 +142,16 @@ impl BowlingGame {
     }
 
     /// Returns the frame and subframe for the points to be added
-    fn get_valid_frame(&self) -> Option<u16> {
+    fn get_valid_frame(&self, pins: u16) -> Option<u16> {
         if self.current_frame < 9 {
             if self.frames[self.current_frame as usize].sub_frames.len() < 2 {
-                Some(self.current_frame)
+                if self.frames[self.current_frame as usize].sub_frames.len() == 1
+                    && self.get_subframes_sum(self.current_frame) == 10
+                {
+                    Some(self.current_frame + 1)
+                } else {
+                    Some(self.current_frame)
+                }
             } else {
                 Some(self.current_frame + 1)
             }
@@ -222,29 +168,29 @@ impl BowlingGame {
 
     fn is_pin_valid(&self, pins: u16) -> bool {
         if self.current_frame < 9 {
-            pins <= (10
-                - self.frames[self.current_frame as usize]
-                    .sub_frames
-                    .iter()
-                    .sum::<u16>())
+            pins <= (10 - self.get_subframes_sum(self.current_frame))
         } else if self.frames[self.current_frame as usize].sub_frames.len() <= 2
             && !self.is_spare(self.current_frame)
         {
-            pins <= (10
-                - self.frames[self.current_frame as usize]
-                    .sub_frames
-                    .iter()
-                    .sum::<u16>())
+            pins <= (10 - self.get_subframes_sum(self.current_frame))
         } else {
             pins <= 10
         }
     }
 
     fn is_spare(&self, frame_index: u16) -> bool {
+        self.get_subframes_sum(frame_index) == 10
+            && self.frames[frame_index as usize].sub_frames.len() == 2
+    }
+
+    fn get_strike_frame(&self) -> Option<&StrikeFrame> {
+        self.strike_frames.iter().find(|&frame| frame.active)
+    }
+
+    fn get_subframes_sum(&self, frame_index: u16) -> u16 {
         self.frames[frame_index as usize]
             .sub_frames
             .iter()
             .sum::<u16>()
-            == 10
     }
 }
