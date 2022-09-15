680b84ae3f1344714c0a77c716c71df4bac31a9b
madclaws <pavanan.anandu63@gmail.com>
2022-09-15 16:11:38 +0530

    feat: bAD POP

--- too-many-linked-lists/src/first.rs
+++ too-many-linked-lists/src/first.rs
@@ -29,14 +29,14 @@ impl List {
     }
 
     pub fn pop(&mut self) -> Option<i32>{
-        match &self.head {
+        match mem::replace(&mut self.head, Link::Empty)  {
             Link::Empty => {
-
+                None
             },
             Link::More(node) => {
-                
+                self.head = node.next;
+                Some(node.elem)                
             }
         }
-        unimplemented!()
     }
 }
