560dd560bbf270930785ba7cc12e2b82d1d00c12
madclaws <pavanan.anandu63@gmail.com>
2022-09-16 22:51:06 +0530

    feat: tests doe BAD linkedlist

--- too-many-linked-lists/src/first.rs
+++ too-many-linked-lists/src/first.rs
@@ -40,3 +40,21 @@ impl List {
         }
     }
 }
+
+mod test {
+    use super::List;
+    #[test]
+    fn basics() {
+        // TODO
+        let mut list = List::new();
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+        assert_eq!(list.pop(), Some(1));
+    }
+}
