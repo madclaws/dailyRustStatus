58059064bd8696a483820770cfa345165fcf68e5
madclaws <pavanan.anandu63@gmail.com>
2022-09-18 00:06:15 +0530

    feat: more tests for BAD linked list

--- too-many-linked-lists/src/first.rs
+++ too-many-linked-lists/src/first.rs
@@ -56,5 +56,14 @@ mod test {
         assert_eq!(list.pop(), Some(3));
         assert_eq!(list.pop(), Some(2));
         assert_eq!(list.pop(), Some(1));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        assert_eq!(list.pop(), None);
+        assert_eq!(list.pop(), None);
     }
 }
